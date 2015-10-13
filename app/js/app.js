50340a7ef6d16f40d044e1415cf87922e9086c76
averma <anil.verma@e2open.com>
2015-10-13 22:33:53 +0530

    testing a service

--- app/js/app.js
+++ app/js/app.js
@@ -57,3 +57,16 @@ angular.module("exampleApp")
 		}
 	}
 });
+
+angular.module("exampleApp")
+.factory("counterService", function () {
+	var counter = 0;
+	return {
+	incrementCounter: function () {
+	counter++;
+	},
+	getCounter: function() {
+	return counter;
+	}
+	}
+	});
